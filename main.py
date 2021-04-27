01117482d559b7407d7f4ac4b93990bbfc189393
Mikael Cassamassimo Borges <66427859+yoskatista@users.noreply.github.com>
2021-04-27 17:40:20 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def display_message(nome):
+    print("Estou aprendendo a usar " + nome)
+display_message("funções")
+
+def favorite_book(livro):
+    print('Meu livro favorito é ' + livro)
+favorite_book("python 3")
